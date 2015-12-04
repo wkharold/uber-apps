c09d2d1100253b67909cf8fe28e9dd74fdc3e29d
Ward K Harold <wkharold@gmail.com>
2015-12-04 06:38:30 -0600

    Start db testing

---
+++ pit/cmd/pitd/db/db_test.go
@@ -0,0 +1,16 @@
+package db
+
+import "testing"
+
+type findertest struct {
+	description string
+}
+
+var (
+	findertests = []findertest{}
+)
+
+func TestFinders(t *testing.T) {
+	for _, ft := range findertests {
+	}
+}
