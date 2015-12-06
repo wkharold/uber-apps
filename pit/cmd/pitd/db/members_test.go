1624de033762aaba2d80310436ab8512bc5d515a
Ward K Harold <wkharold@gmail.com>
2015-12-06 15:00:46 -0600

    Finsh members finder tests

--- pit/cmd/pitd/db/members_test.go
+++ pit/cmd/pitd/db/members_test.go
@@ -16,6 +16,24 @@ type findAllMembersTest struct {
 	err         error
 }
 
+type findMembersByEmailTest struct {
+	description string
+	fn          func(Members, context.Context, string) (Member, error)
+	email       string
+	ctxfn       func() context.Context
+	expected    Member
+	err         error
+}
+
+type findMembersByIDTest struct {
+	description string
+	fn          func(Members, context.Context, int) (Member, error)
+	id          int
+	ctxfn       func() context.Context
+	expected    Member
+	err         error
+}
+
 var (
 	bob   = Member{id: 1003, email: "bob@members.com"}
 	carol = Member{id: 1004, email: "carol@members.com"}
@@ -26,6 +44,18 @@ var (
 		{"FindAll no members", Members.FindAll, emptytables, []Member{}, nil},
 		{"FindAll one member", Members.FindAll, onemember, []Member{bob}, nil},
 		{"FindAll members", Members.FindAll, manymembers, []Member{carol, ted, alice}, nil},
+	}
+	findMembersByEmailTests = []findMembersByEmailTest{
+		{"FindByEmail empty tables", Members.FindByEmail, "bob@members.com", emptytables, Member{}, sql.ErrNoRows},
+		{"FindByEmail many members no match", Members.FindByEmail, "fred.c.dobbs@members.com", manymembers, Member{}, sql.ErrNoRows},
+		{"FindByEmail one member", Members.FindByEmail, "bob@members.com", onemember, bob, nil},
+		{"FindByEmail members", Members.FindByEmail, "ted@members.com", manymembers, ted, nil},
+	}
+	findMembersByIDTests = []findMembersByIDTest{
+		{"FindByID empty tables", Members.FindByID, 1003, emptytables, Member{}, sql.ErrNoRows},
+		{"FindByID many members no match", Members.FindByID, 2001, manymembers, Member{}, sql.ErrNoRows},
+		{"FindByID one member", Members.FindByID, 1003, onemember, bob, nil},
+		{"FindByID members", Members.FindByID, 1005, manymembers, ted, nil},
 	}
 )
 
@@ -45,6 +75,50 @@ func TestFindAllMembers(t *testing.T) {
 			t.Errorf("%s: expected %+v, got %+v", nt.description, nt.expected, ms)
 			dropdb(db)
 			continue
+		}
+
+		dropdb(db)
+	}
+}
+
+func TestFindMembersByEmail(t *testing.T) {
+	for _, nt := range findMembersByEmailTests {
+		ctx := nt.ctxfn()
+		db := ctx.Value("database").(*sql.DB)
+
+		p, err := nt.fn(struct{}{}, ctx, nt.email)
+		switch {
+		case err != nil && err != nt.err:
+			t.Errorf("%s: unexpected error [%+v]", nt.description, err)
+		case err != nil && err == nt.err:
+			break
+		default:
+			if p != nt.expected {
+				t.Errorf("%s: expected %+v, got %+v", nt.description, nt.expected, p)
+				break
+			}
+		}
+
+		dropdb(db)
+	}
+}
+
+func TestFindMembersByID(t *testing.T) {
+	for _, nt := range findMembersByIDTests {
+		ctx := nt.ctxfn()
+		db := ctx.Value("database").(*sql.DB)
+
+		p, err := nt.fn(struct{}{}, ctx, nt.id)
+		switch {
+		case err != nil && err != nt.err:
+			t.Errorf("%s: unexpected error [%+v]", nt.description, err)
+		case err != nil && err == nt.err:
+			break
+		default:
+			if p != nt.expected {
+				t.Errorf("%s: expected %+v, got %+v", nt.description, nt.expected, p)
+				break
+			}
 		}
 
 		dropdb(db)
